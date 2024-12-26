999007cd08946546126e77d035d54764b3242555
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-26 09:37:21 +0000

    add

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,18 @@
-CREATE TABLE Lessons (
+DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS modules;
+DROP TABLE IF EXISTS programs;
+
+CREATE TABLE courses (
+    id bigint PRIMARY KEY,
+    name varchar(255),
+    description text,
+    created_at timestamp,
+    updated_at timestamp,
+    is_deleted tinyint default 0
+);
+
+CREATE TABLE lessons (
     id bigint PRIMARY KEY,
     name varchar(255),
     content text,
@@ -6,5 +20,24 @@ CREATE TABLE Lessons (
     position bigint,
     created_at timestamp,
     updated_at timestamp,
-    url_course varchar(255)
+    course_id bigint REFERENCES courses (id),
+    is_deleted tinyint default 0
+);
+
+CREATE TABLE modules (
+    id bigint PRIMARY KEY,
+    name varchar(255),
+    description text,
+    created_at timestamp,
+    updated_at timestamp,
+    is_deleted tinyint default 0
+);
+
+CREATE TABLE programs (
+    id bigint PRIMARY KEY,
+    name varchar(255),
+    price int,
+    type varchar(255),
+    created_at timestamp,
+    updated_at timestamp
 );
