c36d915a9e830f5c4868cc7ba14a499ce29fd414
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-26 10:28:51 +0000

    add

--- sql/insert_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Lessons VALUES (
-    1, 'First', 'The big big text', 'https://ru.hexlet.io/projects/136/members/43407', 1, '2024-10-24', '2024-12-26', 'https://translate.google.ru/'
-);
